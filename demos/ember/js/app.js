2bd67f856102cf5009a298ae440c64a05cb48a91
marcysutton <holla@marcysutton.com>
2014-07-28 21:34:59 -0700

    Updating Ember example

--- demos/ember/js/app.js
+++ demos/ember/js/app.js
@@ -1,7 +1,15 @@
 App = Ember.Application.create();
 
 App.GoldHotpantsComponent = Ember.Component.extend({
-  tagName: 'section'
+	attributeBindings: [
+		'tabIndex'
+	],
+	tagName: 'div',
+	ariaRole: 'form',
+
+	tabIndex: function() {
+		return this.get('active') ? 0 : -1;
+	}.property('active')
 });
 
 App.IndexRoute = Ember.Route.extend({
