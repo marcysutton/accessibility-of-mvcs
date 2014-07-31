906591936d2f7df04a5f26fb833921eee62f1430
marcysutton <holla@marcysutton.com>
2014-07-31 14:05:42 -0700

    Ember demo takes input

--- demos/ember/js/app.js
+++ demos/ember/js/app.js
@@ -1,25 +1,55 @@
 App = Ember.Application.create();
 
+Ember.RadioButton = Ember.View.extend({  
+    tagName : "input",
+    type : "radio",
+    attributeBindings : [ "name", "type", "value", "checked:checked:" ],
+    click : function() {
+      this.set("selection", this.$().val())
+    },
+    checked : function() {
+      return this.get("value") == this.get("selection");   
+    }.property()
+});
+
 App.GoldHotpantsComponent = Ember.Component.extend({
+	captions: [{
+	  body: "Ow! My eye."
+	}, {
+	  body: "Shiny!"
+	}],
+
+	winningCaption: "Tom Dale's Gold Shorts",
+  
+  radioButtonSelected: 1,
+	
 	attributeBindings: [
 		'tabIndex'
 	],
 	tagName: 'div',
 	ariaRole: 'form',
 
+	winningCaptionDidChange: function(sender, key, value, rev){
+		// do something fancy here
+	},
+
 	tabIndex: function() {
 		return this.get('active') ? 0 : -1;
-	}.property('active')
-});
+	}.property('active'),
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return {
-    	captions: [{
-			  body: "Ow! My eye."
-			}, {
-			  body: "Shiny!"
-			}]
+	createCaption: function() {
+		// Get the caption set by the "New Caption" text field
+		var caption = this.get('newCaption');
+		if (!caption.trim()) {
+			return;
 		}
-  }
+		// Create the new Caption model
+		this.get('captions').pushObject({body: caption});
+
+		// Clear the "New Caption" text field
+		this.set('newCaption', '');
+
+		// Save the new model
+		// this.get('store').commit();
+	}
 });
